16bef5d2e2818fc2bbad1870351dea4f6545395b
Itay-Hefetz <hefetz99@gmail.com>
2018-05-13 17:56:41 +0300

    GREEN: add test

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -4,7 +4,15 @@ function sum (a, b) {
   return a+b;
 }
 
+function sub (a, b) {
+  return a-b;
+}
 test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.end();
+});
+
+test('sub should return the sub of two numbers', function (t) {
+  t.equal(1, sub(2, 1)); // make this test pass by completing the sub function!
   t.end();
 });
